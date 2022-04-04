fb2088e5f9b45a2d369cedfeb6b043b8f27ed43b
Juho-PC\Juho <savinainen.juho@gmail.com>
2022-04-04 11:46:48 +0300

    lisäsin enemy kissa parempi move.

---
+++ Peliprojekti/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public GameObject detectionPoint;
+    public Animator animator;
+
+    // Tämä on oikea tapa. 
+    [SerializeField]
+    private float direction;
+
+    [SerializeField]
+    private bool changeDir;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Time.deltaTime * direction, 0, 0);
+        transform.localScale = new Vector3(direction, 1, 1);
+    }
+    private void LateUpdate()
+    {
+
+        Debug.DrawRay(detectionPoint.transform.position, Vector2.down, Color.green);
+
+        RaycastHit2D hit = Physics2D.Raycast(detectionPoint.transform.position, Vector2.down, 1, groundLayer);
+
+        if (hit.collider == null)
+        {
+            ChangeDirection();
+        }
+
+        Debug.DrawRay(detectionPoint.transform.position, Vector2.right * direction * 0.2f, Color.blue);
+
+        RaycastHit2D hit2 = Physics2D.Raycast(detectionPoint.transform.position, Vector2.right * direction, 0.2f, groundLayer);
+
+        if (hit2.collider != null)
+        {
+            ChangeDirection();
+        }
+
+
+
+    }
+
+    void ChangeDirection()
+    {
+
+        direction *= -1;
+
+    }
+    public void Die()
+    {
+        animator.SetTrigger("Die");
+        moveSpeed = 0;
+        Destroy(GetComponent<Rigidbody2D>());
+        Destroy(GetComponent<BoxCollider2D>());
+        Destroy(gameObject, 1);
+    }
+
+}
